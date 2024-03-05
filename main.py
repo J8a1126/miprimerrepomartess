994f76ecfbb4c3f2027d98a33cd2bad0e4dfabfa
jhonyochoa <ochoajhony1126@gmail.com>
2024-03-04 23:36:58 -0500

    nt3 feat se programan condiciones del login

---
+++ main.py
@@ -0,0 +1,11 @@
+#comentario de linea 
+'''
+soy un comentario de de bloque
+'''
+
+#DATOS PRIMITIVOS
+nombreUsuario = 'Jhony'
+edadusuario = 31
+estaturaUsuario = 1.80
+estaLloviendo = True
+notaPromedio = None
